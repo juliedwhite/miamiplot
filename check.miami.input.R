4e91e724aebf26dea9963a4d22fd4b9daa4a9e60
Julie White <whitejuliedanielle@gmail.com>
2020-04-30 14:24:24 -0400

    Remove references to df and just use data

--- check.miami.input.R
+++ check.miami.input.R
@@ -2,19 +2,16 @@
 
 check.miami.input <- function(data, split.by, split.at) {
   require(checkmate)
-  
-  # Rename data
-  df <- data
-  
+
   # Establish a new argument check object
   check <- makeAssertCollection()
   
-  # Check that df is a dataframe
-  assertDataFrame(df, col.names = "named", add = check)
+  # Check that data is a dataframe
+  assertDataFrame(data, col.names = "named", add = check)
   
   # The user should supply the column name by which they want to split their data using split.by.
   # For example: beta values, study name, genotype status. 
-  assertNames(split.by, type = "named", subset.of = colnames(df),  add = check)
+  assertNames(split.by, type = "named", subset.of = colnames(data),  add = check)
   
   # The user should supply the point at which they want to split their data using split.at, as an integer or character.
   # For example: if they want positive beta values on top, then split.at = 0
