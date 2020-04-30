a27bb3204d6821ddc8b85c81ac2611d1e89761ec
Julie White <whitejuliedanielle@gmail.com>
2020-04-30 15:29:19 -0400

    Add base plots

--- plot.miami.R
+++ plot.miami.R
@@ -60,6 +60,9 @@ plot.miami <- function(data, split.by, split.at, chr="chr", pos="pos", p = "P") 
     #Find the center of the chromosome
     summarize(chr.center=(max(rel.pos) + min(rel.pos))/2)
   
+  # To create a symmetric looking plot, calculate the maximum p-value
+  maxp <- ceiling(max(-log10(data[,p.indx])))
+  
   # Depending on what the user has input for split.by and split.at, make top and bottom data.
   if(is.numeric(split.at)){
     # Create top data using numeric info.
@@ -77,8 +80,30 @@ plot.miami <- function(data, split.by, split.at, chr="chr", pos="pos", p = "P") 
       filter(!!sym(split.by) != split.at)
   }
   
+  # Create base top plot
+  top.plot <- ggplot(data = top.data, aes(x=rel.pos, y=-log10(!!sym(p.name)))) +
+    geom_point(aes(color=as.factor(!!sym(chr.name))), size=0.25) +
+    scale_color_manual(values = rep(c("black", "grey"), nrow(axis.data))) +
+    scale_x_continuous(labels = pull(axis.data[,1]), breaks = axis.data$chr.center, expand = expansion(mult=0.01)) +
+    scale_y_continuous(limits = c(0, maxp), expand = expansion(mult = c(0.02,0))) + 
+    labs(x = "", y = expression('-log'[10]*'(P)')) +
+    theme_classic() +
+    theme(legend.position = "none", axis.title.x = element_blank(), plot.margin = margin(b=0))
+ 
+  # Create base bottom plot
+  bottom.plot <- ggplot(data = bot.data, aes(x=rel.pos, y=-log10(!!sym(p.name)))) +
+    geom_point(aes(color=as.factor(!!sym(chr.name))), size=0.25) +
+    scale_color_manual(values = rep(c("black", "grey"), nrow(axis.data))) +
+    scale_x_continuous(breaks = axis.data$chr.center, position = "top", expand = expansion(mult=0.01)) +
+    scale_y_reverse(limits = c(maxp, 0), expand = expansion(mult = c(0,0.02))) + 
+    labs(x = "", y = expression('-log'[10]*'(P)')) + 
+    theme_classic() +
+    theme(legend.position = "none", axis.text.x = element_blank(), axis.title.x = element_blank(), plot.margin = margin(t=0))
   
-  
+  # Put the two together
+  p <- top.plot + bottom.plot + plot_layout(ncol=1)
+
+  return(p)
   
 }
 
