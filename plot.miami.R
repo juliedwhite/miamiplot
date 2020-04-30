08f8b962ce6c9e89bc4272d24b3e03e9f0532b47
Julie White <whitejuliedanielle@gmail.com>
2020-04-30 14:53:22 -0400

    Move checking sections to helper functions

--- plot.miami.R
+++ plot.miami.R
@@ -11,60 +11,26 @@ plot.miami <- function(data, split.by, split.at, chr="chr", pos="pos") {
   require(patchwork)
   require(ggrepel)
   
-  # Rename data
-  df <- data
-  
   # Establish a new argument check object
   check <- makeAssertCollection()
   
   # Check the input 
   check.miami.input(data = data, split.by = split.by, split.at = split.at)
   
-  # Identify column containing chromosome information.
-  if(testNames(chr, type = "named", subset.of = colnames(df))) {
-    # Chromosome column specified and is subset of colnames. Find column index matching this value.
-    chr.name <- chr
-    chr.indx <- which(colnames(df)==chr.name)
-  } else {
-    # Chromosome column name is not in the right format or is not a subset of colnames. 
-    assertNames(chr, type = "named", subset.of = colnames(df), add = check)
-  }
+  # Identify column index for chromosome information. 
+  chr.indx <- find.col.indx(data = data, x = chr)
+  chr.name <- chr
   
-  if(testNames(pos, type = "named", subset.of = colnames(df))) {
-    # Position column specified is a subset of colnames. Find column index matching this value.
-    pos.name <- pos
-    pos.indx <- which(colnames(df)==pos.name)
-  } else {
-    # Position column name is not in the right format or is not a subset of colnames. 
-    assertNames(pos, type = "named", subset.of = colnames(df), add = check)
-  }
+  # Identify column index for position information
+  pos.indx <- find.col.indx(data = data, x = pos)
+  pos.name <- pos
   
-  # At this point, check if there are any errors. 
-  if(!check$isEmpty()) {
-    reportAssertions(check)
-  }
-  
-  # Test if chromosome column is numeric. 
-  if(!testMultiClass(df[,chr.indx], classes = c("numeric", "integer"))){
-    check$push(paste0("Your chromosome column is of class ",
-                      class(df[,chr.indx]),
-                      ". Please make sure your chromosome column is numeric or integer."))
-  }
-
-  # Test if position column is numeric. 
-  if(!testMultiClass(df[,pos.indx], classes = c("numeric", "integer"))){
-    check$push(paste0("Your position column is of class ",
-                      class(df[,pos.indx]),
-                      ". Please make sure your position column is numeric or integer."))
-  }
-
-  # At this point, check if there are any errors. 
-  if(!check$isEmpty()) {
-    reportAssertions(check)
-  }
+  # Test if chromosome and position columns are numeric or integer. 
+  check.miami.numeric(data = data, col.indx = chr.indx)
+  check.miami.numeric(data = data, col.indx = pos.indx)
   
   # To make the plot, we need to know the cumulative position of each probe/snp across the whole genome.
-  df <- df %>% 
+  data <- data %>% 
     #Group by chromosome
     group_by(!!sym(chr.name)) %>% 
     # Compute chromosome size
@@ -74,7 +40,7 @@ plot.miami <- function(data, split.by, split.at, chr="chr", pos="pos") {
     # Remove chr length column
     select(-chrlength) %>%
     # Temporarily add the cumulative length of each chromosome to the initial dataset 
-    left_join(df, ., by=chr.name) %>%
+    left_join(data, ., by=chr.name) %>%
     # Sort by chr then position 
     arrange(!!sym(chr.name), !!sym(pos.name)) %>%
     # Add the position to the cumulative chromosome length to get the position of this probe relative to all other probes
@@ -83,30 +49,30 @@ plot.miami <- function(data, split.by, split.at, chr="chr", pos="pos") {
     select(-cumulativechrlength)
   
   # However, we don't want to print the cumulative position on the x-axis, so we need to provide the chromosome position labels relative to the entire genome.
-  axis.df = df %>% 
+  axis.data = data %>% 
     #Group by chromosome
     group_by(!!sym(chr.name)) %>% 
     #Find the center of the chromosome
     summarize(chr.center=(max(rel.pos) + min(rel.pos))/2)
   
-  # Depending on what the user has input for split.by and split.at, make top and bottom df.
+  # Depending on what the user has input for split.by and split.at, make top and bottom data.
   if(is.numeric(split.at)){
-    # Create top df using numeric info.
-    top.df <- df %>%
+    # Create top data using numeric info.
+    top.data <- data %>%
       filter(!!sym(split.by) >= split.at)
-    # Create bottom df using numeric info
-    bot.df <- df %>%
+    # Create bottom data using numeric info
+    bot.data <- data %>%
       filter(!!sym(split.by) < split.at)
   } else if(is.character(split.at)){
-    # Create top df using character specified by user
-    top.df <- df %>%
+    # Create top data using character specified by user
+    top.data <- data %>%
       filter(!!sym(split.by) == split.at)
-    # Create bottom df using character not specified by user
-    bot.df <- df %>%
+    # Create bottom data using character not specified by user
+    bot.data <- data %>%
       filter(!!sym(split.by) != split.at)
   }
   
-
+  return(c(nrow(top.data), nrow(bot.data)))
 }
 
 
