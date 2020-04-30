8496d97197242db947fddb9aa22d824f18272129
Julie White <whitejuliedanielle@gmail.com>
2020-04-30 10:52:37 -0400

    Use checkmate for argument checking
    Checks that df = dataframe, split.by is named and a subset of the df column names, split.at is either character or numeric with length = 1.

--- plot.miami.R
+++ plot.miami.R
@@ -5,6 +5,7 @@
 plot.miami <- function(data, split.by, split.at, chr=NULL, pos=NULL) {
   # Necessary packages
   require(rlang)
+  require(checkmate)
   require(dplyr)
   require(ggplot2)
   require(patchwork)
@@ -13,17 +14,28 @@ plot.miami <- function(data, split.by, split.at, chr=NULL, pos=NULL) {
   # Rename data
   df <- data
   
-  # Check that it is a dataframe
-  if(!is.data.frame(df)){
-    stop("Please supply a data.frame object, ideally with column names specified.")
-  }
+  # Establish a new argument check object
+  check <- makeAssertCollection()
   
-  # The user should supply the column name by which they want to split their data using split.by. 
-    # For example, beta values, study name, genotyped vs. imputed status. 
-  if(is.null(split.by)) {
-    stop("Please specify the column name that you want to use to split your Miami plot.")
-  }
-
+  # Check that df is a dataframe
+  assertDataFrame(df, col.names = "named", add = check)
+  
+  # The user should supply the column name by which they want to split their data using split.by.
+    # For example: beta values, study name, genotype status. 
+  assertNames(split.by, type = "named", subset.of = colnames(df),  add = check)
+  
+  # The user should supply the point at which they want to split their data using split.at, as an integer or character.
+    # For example: if they want positive beta values on top, then split.at = 0
+    # For example, if they want "genotyped" SNPs on top, then split.at = "genotyped"
+  # If the user has supplied a single character for split.at
+  if(!testCharacter(split.at, len = 1)){
+    if(!testNumeric(split.at, len = 1)){
+      check$push("split.at must be either character or numeric, with length = 1")
+    }
+  } 
+  
+  reportAssertions(check)
+  
   # Identify the column containing chromosome information.
   if(!is.null(chr)){
     # Chromosome column specified. Find column index matching this value.
